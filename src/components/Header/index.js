14a088cf65db0ff4eeb4d8c5967beb387c922249
Roxolana <roxolanapavlyk@gmail.com>
2019-11-03 21:51:27 +0100

    install react-router-dom for navigation purposes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { Root, Wrapper, Logo } from "./styles";
+import Navigation from "./Navigation/Navigation";
 
 export const Header = () => (
   <Root>
     <Wrapper>
       <Logo />
+      <Navigation />
     </Wrapper>
   </Root>
 );
