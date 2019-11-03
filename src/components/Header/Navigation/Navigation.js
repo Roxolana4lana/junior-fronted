fa7873d8e515bb3ec5cc8322460ed146d8c4df53
Roxolana <roxolanapavlyk@gmail.com>
2019-11-03 21:50:10 +0100

    add navigation component

---
+++ src/components/Header/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <ul>
+        <li>
+          <Link to="/">Start</Link>
+        </li>
+        <li>
+          <Link to="/">My Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default withRouter(Navigation);
